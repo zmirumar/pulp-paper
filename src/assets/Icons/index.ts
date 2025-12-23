b3eb530f51a5b6e623b6420dc14ad779704dbe83
Muhammadaminxudoyberdiyev <muhammadaminxudoyberdiyev62@gmail.com>
2025-12-23 16:52:21 +0500

    fix bags

--- src/assets/Icons/index.ts
+++ src/assets/Icons/index.ts
@@ -11,5 +11,4 @@ export { default as SearchOutLined } from "./SearchOutlined.svg";
 export { default as CheckingIcon} from "./ic_check.svg"
 export { default as NoInternetIcon} from './NoConection.svg'
 export { default as ProblemIcon} from './Undifiend.svg'
-export { default as PlusIcon} from './PlusIcon.svg'
 
