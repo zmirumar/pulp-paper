0ff47b8bcd3b3e9bb5ad5768216371934da6511a
Muhammadaminxudoyberdiyev <muhammadaminxudoyberdiyev62@gmail.com>
2025-12-23 15:34:07 +0500

    user page updated

--- src/features/users/components/userForm/style.ts
+++ src/features/users/components/userForm/style.ts
@@ -6,13 +6,18 @@ export const UserFormStyled = styled.div`
   }
   .checkbox-wrapper {
     border-bottom: 1px solid #f0f0f0;
-    margin: 12px;
+    &:last-child {
+      border-bottom: none;
+    }
   }
   .add-btn {
     width: 162px;
   }
   .ant-table-row-selected {
     background-color: red !important;
+  }
+  .ant-input:focus::placeholder{
+    color: transparent !important;
   }
   .form-btns {
     display: flex;
@@ -26,6 +31,17 @@ export const UserFormStyled = styled.div`
 `;
 
 export const UserFormStyledBtns = styled.div`
+  position: relative;
+  padding: 4px 0;
+  &::before {
+    content: "";
+    position: absolute;
+    height: 1px;
+    background-color: #f0f0f0;
+    width: 100%;
+    z-index: 5;
+    bottom: 47px;
+  }
   display: flex;
   justify-content: end;
   gap: 12px;
